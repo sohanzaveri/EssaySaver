be6ae5a19ea31b638e4354f1530d875fd423b316
sohanzaveri <zaveri0@purdue.edu>
2019-10-12 14:57:56 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,47 @@
+
+(function (){
+  let count = 0;
+
+  document.addEventListener("keypress", function()
+  {
+    count++;
+
+  });
+
+  window.setInterval(function(){
+
+    console.log(count);
+
+    count=0;
+
+  }, 1000);
+})();
+
+
+
+
+
+
+
+
+
+
+
+chrome.runtime.onMessage.addListener(function gotMessage(message, sender, sendResponse)
+{
+
+  alert("hey")
+
+  if(message.txt === "hello"){
+
+    let para= document.getElementsByTagName("p");
+
+    for(x of para)
+    {
+      x.style['background-color'] = '#FF00FF';
+    }
+
+  }
+
+
+});
---
+++ popup.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+document.getElementById('New Goal').addEventListener("click", function()
+{
+
+
+
+});
+
+
+document.getElementById('Goal').addEventListener("click", function()
+{
+
+
+
+});
+
+
+
+document.getElementById('Count').addEventListener("click", function()
+{
+
+  alert("popup works");
+
+
+});
