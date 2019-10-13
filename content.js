a5b9eb8c97d87f07556e69c3e1015b1b5d9d5fe7
shelzzz <32962319+shelzzz@users.noreply.github.com>
2019-10-12 21:25:31 -0400

    version 2 of extension( added some UI and timer)

--- content.js
+++ content.js
@@ -1,32 +1,21 @@
-
-
-(function (){
-
-
-  let counts = [];
-  let count = 0;
-
-  document.body.addEventListener("keydown", function()
-  {
-    count++;
-  });
-
-  window.setInterval(function(){
-
-    //why doesnt counts add everthing to an array
-    counts.push(count);
-
-    console.log(counts)
-
-    count=0;
-
-  }, 60000);
-})();
-
-
-
-
-//chrome.tabs.onActivated.addListener(function callback)
+//
+// (function (){
+//   let count = 0;
+//
+//   document.addEventListener("keypress", function()
+//   {
+//     count++;
+//
+//   });
+//
+//   window.setInterval(function(){
+//
+//     console.log(count);
+//
+//     count=0;
+//
+//   }, 1000);
+// })();
 
 
 
@@ -37,95 +26,6 @@
 
 
 
-
-/*
-window.onload = function()
-{
-  els = document.getElementsByTagName("*");
-  for(i=0;i<els.length;i++) {
-    els[i].onkeypress=function(){
-      alert("hello!");
-    };
-  }
-}
-*/
-
-
-/*
-  var iframe = document.getElementsByTagName("iframe")[0];
-  if (iframe) {
-
-      iframe.contentDocument.addEventListener("keypress", function(evt) {
-          console.log("iframe keypress: " + evt.which);
-
-          alert("IT WORKSS");
-
-      }, false);
-  }
-  */
-
-/*
-  if (!window.onkeydown) {
-    const data = {
-      count: 0,
-    };
-    window.onkeydown = () => {
-      data.count++;
-    };
-
-    window.setInterval(() => {
-      console.log(data.count);
-    }, 1000);
-  }
-}
-*/
-
-
-
-
-/*
-var iframe = document.getElementsByTagName("iframe")[0];
-if (iframe) {
-  alert("working");
-    iframe.contentDocument.addEventListener("keydown", function(evt) {
-      alert("not working");
-
-        console.log("iframe keypress: " + evt.which);
-    }, false);
-}
-
-/***
-(function (){
-
-
-  var iframe = document.getElementsByTagName("iframe")[0];
-  if (iframe) {
-
-      iframe.contentDocument.addEventListener("keypress", function(evt) {
-          console.log("iframe keypress: " + evt.which);
-
-          alert("IT WORKSS");
-
-      }, false);
-  }
-
-
-  let count = 0;
-
-  document.addEventListener("keypress", function()
-  {
-    count++;
-
-  });
-
-  window.setInterval(function(){
-
-    console.log(count);
-
-    count=0;
-
-  }, 1000);
-})();
 
 chrome.runtime.onMessage.addListener(function gotMessage(message, sender, sendResponse)
 {
@@ -142,6 +42,6 @@ chrome.runtime.onMessage.addListener(function gotMessage(message, sender, sendRe
     }
 
   }
-});
 
-***/
+
+});
--- popup.js
+++ popup.js
@@ -1,32 +1,43 @@
+// document.getElementById('New Goal').addEventListener("click", function()
+// {
+//
+//
+//
+// });
+//
+//
+// document.getElementById('Goal').addEventListener("click", function()
+// {
+//
+//
+//
+// });
+//
+//
+//
+// document.getElementById('Count').addEventListener("click", function()
+// {
+//
+//   alert("popup works");
+//
+//
+// });
+var startTime, endTime;
 
+function start() {
+  startTime = new Date();
+};
 
+function end() {
+  endTime = new Date();
+  var timeDiff = endTime - startTime; //in ms
+  // strip the ms
+  timeDiff /= 1000;
 
+  // get seconds
+  var seconds = Math.round(timeDiff);
+  console.log(seconds + " seconds");
+}
 
-
-
-
-
-document.getElementById('New Goal').addEventListener("click", function()
-{
-
-
-
-});
-
-
-document.getElementById('Goal').addEventListener("click", function()
-{
-
-
-
-});
-
-
-
-document.getElementById('Count').addEventListener("click", function()
-{
-
-  alert("popup works");
-
-
-});
+document.getElementById('start').addEventListener('click', start);
+document.getElementById('end').addEventListener('click', end);
